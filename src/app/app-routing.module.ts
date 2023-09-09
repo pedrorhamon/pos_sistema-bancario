6bcf08f76d055b1ff0eab797ad0f01a0be92b36a
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-09-09 20:50:53 -0300

    Criando roteamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListagemClienteComponent } from './components/listagem-cliente/listagem-cliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "clientes",
+    component: ListagemClienteComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
