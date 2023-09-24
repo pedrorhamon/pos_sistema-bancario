e62a113da00453e9032613a3cf1d116a738ec66c
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-09-24 16:50:59 -0300

    concluindo crud de contas

--- src/app/components/contas/salvar-contas/salvar-contas.component.ts
+++ src/app/components/contas/salvar-contas/salvar-contas.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { ClienteService } from 'src/app/shared/cliente.service';
+import { ContasService } from 'src/app/shared/contas.service';
+import { Conta } from 'src/app/shared/model/conta';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-salvar-contas',
@@ -12,7 +14,7 @@ export class SalvarContasComponent implements OnInit {
 
   formGroup: FormGroup;
 
-  constructor(private clienteService: ClienteService, private router: Router) {
+  constructor(private contaService: ContasService, private router: Router) {
     this.formGroup = new FormGroup({
       id: new FormControl(null),
       numero: new FormControl('', Validators.required),
@@ -22,7 +24,28 @@ export class SalvarContasComponent implements OnInit {
     });
    }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  cadastrar() {
+    const conta: Conta = this.formGroup.value;
+    this.contaService.salvarConta(conta).subscribe({
+      next: () => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Sucesso',
+          text: 'Conta cadastrado com sucesso!',
+          showConfirmButton: false,
+          timer: 1500
+        })
+        this.router.navigate(['/contas'])
+      },
+      error: (error) => {
+        console.error(error)
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Erro ao cadastrar cliente!',
+        })
+      }})
+  }
 }
