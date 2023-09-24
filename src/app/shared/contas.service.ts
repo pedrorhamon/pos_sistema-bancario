33c1ebf40c007fd55638dfd55737d00f44b9970f
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-09-24 16:15:23 -0300

    Criando os metodos no service

--- src/app/shared/contas.service.ts
+++ src/app/shared/contas.service.ts
@@ -1,5 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+
+import { Conta } from './model/conta';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +12,34 @@ export class ContasService {
 
   api = `${environment.api}/contas/`;
 
-  constructor() { }
+  constructor(private httpCliente: HttpClient) { }
+
+  listarContas(): Observable<Conta[]> {
+    return this.httpCliente.get<Conta[]>(this.api);
+  }
+
+  salvarConta(novaConta: Conta): Observable<Conta> {
+    return this.httpCliente.post<Conta>(
+      this.api, novaConta);
+  }
+
+  listar_paginado(page: number, pageSize: number): Observable<Conta[]> {
+    return this.httpCliente
+    .get<Conta[]>(`${this.api}?page=${page}&pageSize=${pageSize}`);
+  }
+
+
+  deletar(idConta: number): Observable<object> {
+    return this.httpCliente.delete(`${this.api}${idConta}`);
+  }
+
+
+  pesquisarPorId(id: number): Observable<Conta> {
+    return this.httpCliente.get<Conta>(`${this.api}${id}`);
+  }
+
+
+  atualizar(conta: Conta): Observable<Conta> {
+    return this.httpCliente.put<Conta>(`${this.api}${conta.id}`, conta);
+  }
 }
