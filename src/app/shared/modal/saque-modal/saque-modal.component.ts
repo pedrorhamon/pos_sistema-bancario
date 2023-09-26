52daf0eba812e817b60cb0bdc56225c7ac98b6bc
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-09-26 20:26:26 -0300

    adicionando function

--- src/app/shared/modal/saque-modal/saque-modal.component.ts
+++ src/app/shared/modal/saque-modal/saque-modal.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
+import { ClienteService } from '../../cliente.service';
+import { ContasService } from '../../contas.service';
+import { Conta } from '../../model/conta';
+import { SaqueDeposito } from '../../model/saque-deposito';
 
 @Component({
   selector: 'app-saque-modal',
@@ -7,9 +15,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SaqueModalComponent implements OnInit {
 
-  constructor() { }
+  formGroup: FormGroup;
+  contas: Conta[]
 
-  ngOnInit(): void {
+  constructor(private contaService: ContasService, private router: Router, private clienteService: ClienteService){
+
+    this.formGroup = new FormGroup({
+      valor: new FormControl('', Validators.required),
+      conta: new FormControl('', Validators.required)
+    });
+    this.contas = []
   }
+  ngOnInit(): void {
+    this.listarContas()
+  }
+
+  listarContas(): void{
+    this.contaService.listarContas().subscribe(contas => {
+      this.clienteService.listarCliente().subscribe(clientes => {
+        const contasComNomesDeClientes = contas.map(conta => {
+          const cliente = clientes.find(cliente => cliente.id === conta.cliente);
+          if (cliente) {
+            conta.nomeCliente = cliente.nome;
+          }
+          return conta;
+        });
+        this.contas = contasComNomesDeClientes;
+      });
+    })
+  }
+
+  cadastrar() {
+    const saque: SaqueDeposito = this.formGroup.value;
+      // Modo de criação
+      this.contaService.saque(saque).subscribe({
+        next: () => {
+          Swal.fire({
+            icon: 'success',
+            title: 'Sucesso',
+            text: 'Saque registrado com sucesso!',
+            showConfirmButton: false,
+            timer: 1500
+          })
+          this.router.navigate(['/conta']);
+        },
+        error: (error) => {
+          console.error(error);
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Erro ao registrar saque!',
+          });
+        }
+      });
+    }
 
 }
--- src/app/shared/modal/transferencia-modal/transferencia-modal.component.ts
+++ src/app/shared/modal/transferencia-modal/transferencia-modal.component.ts
@@ -1,15 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { ClienteService } from '../../cliente.service';
+import { Conta } from '../../model/conta';
+import { Transferencia } from '../../model/transferencia';
+import { ContasService } from '../../contas.service';
 
 @Component({
   selector: 'app-transferencia-modal',
   templateUrl: './transferencia-modal.component.html',
   styleUrls: ['./transferencia-modal.component.css']
 })
-export class TransferenciaModalComponent implements OnInit {
+export class TransferenciaModalComponent {
+  formGroup: FormGroup;
+  contas: Conta[]
 
-  constructor() { }
+  constructor(private contaService: ContasService, private router: Router, private clienteService: ClienteService){
 
-  ngOnInit(): void {
+    this.formGroup = new FormGroup({
+      valor: new FormControl('', Validators.required),
+      conta_destino: new FormControl('', Validators.required),
+      conta_origem: new FormControl('', Validators.required)
+    });
+    this.contas = []
   }
+  ngOnInit(): void {
+    this.listarContas()
+  }
+
+  listarContas(): void{
+    this.contaService.listarContas().subscribe(contas => {
+      this.clienteService.listarCliente().subscribe(clientes => {
+        const contasComNomesDeClientes = contas.map(conta => {
+          const cliente = clientes.find(cliente => cliente.id === conta.cliente);
+          if (cliente) {
+            conta.nomeCliente = cliente.nome;
+          }
+          return conta;
+        });
+        this.contas = contasComNomesDeClientes;
+      });
+    })
+  }
+
+  cadastrar() {
+    const tranferencia: Transferencia = this.formGroup.value;
+      this.contaService.transferencia(tranferencia).subscribe({
+        next: () => {
+          Swal.fire({
+            icon: 'success',
+            title: 'Sucesso',
+            text: 'Transferência registrada com sucesso!',
+            showConfirmButton: false,
+            timer: 1500
+          })
+          this.router.navigate(['/conta']);
+        },
+        error: (error) => {
+          console.error(error);
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Erro ao registrar transferência!',
+          });
+        }
+      });
+    }
 
 }
