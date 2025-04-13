864c0f259108a95407c7d0ff20e9cc510701aef0
Pedro Rhamon <pedrorhamon16@gmail.com>
2025-04-13 12:00:00 -0300

    Ajustes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,7 +10,7 @@ describe('AppComponent', () => {
         RouterTestingModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
     }).compileComponents();
   });
